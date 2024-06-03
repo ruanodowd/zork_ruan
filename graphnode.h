9c5182a6bffae1100524b68bfe4ea858c5f1bfe5
ruqn <59122094+ruanodowd@users.noreply.github.com>
2024-06-03 12:48:14 +0200

    changes

--- graphnode.h
+++ graphnode.h
@@ -1,10 +1,92 @@
 #ifndef GRAPHNODE_H
 #define GRAPHNODE_H
 
-class graphnode
-{
+#include <QString>
+#include <QVector>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <memory>
+#include <exception>
+#include <iostream>
+#include <bitset>
+
+namespace GraphParser {
+
+// Forward declarations
+class GraphNode;
+
+// Custom exception class
+class ParseException : public std::exception {
 public:
-    graphnode();
+    const char* what() const noexcept override {
+        return "Error parsing JSON";
+    }
 };
+
+// Union for storing different types of data in a node
+union NodeData {
+    int intValue;
+    double doubleValue;
+    bool boolValue;
+    QString* strValue;
+
+    NodeData() : intValue(0) {}
+    ~NodeData() {}
+};
+
+// Abstract base class
+class JSONSerializable {
+public:
+    virtual void fromJSON(const QJsonObject& jsonObj) = 0;
+    virtual QJsonObject toJSON() const = 0;
+    virtual ~JSONSerializable() = default;
+};
+
+class GraphNode : public JSONSerializable {
+public:
+    GraphNode(const QString& id = "") : id(id), data(), edges() {}
+
+    GraphNode(const GraphNode& other); // Copy constructor
+    GraphNode& operator=(const GraphNode& other); // Assignment operator
+
+    virtual ~GraphNode();
+
+    void addEdge(std::shared_ptr<GraphNode> node);
+    void setData(const NodeData& newData);
+
+    const QString& getId() const;
+    const NodeData& getData() const;
+    const QVector<std::shared_ptr<GraphNode>>& getEdges() const;
+
+    void fromJSON(const QJsonObject& jsonObj) override;
+    QJsonObject toJSON() const override;
+
+    // Friend declaration
+    friend std::ostream& operator<<(std::ostream& os, const GraphNode& node);
+
+protected:
+    QString id;
+    NodeData data;
+    QVector<std::shared_ptr<GraphNode>> edges;
+
+    void copyData(const GraphNode& other);
+};
+
+// Template declaration
+template<typename T>
+class JSONParser {
+public:
+    static T parse(const QJsonObject& jsonObj);
+};
+
+// Template specialization for GraphNode
+template<>
+class JSONParser<GraphNode> {
+public:
+    static std::shared_ptr<GraphNode> parse(const QJsonObject& jsonObj);
+};
+
+} // namespace GraphParser
 
 #endif // GRAPHNODE_H
